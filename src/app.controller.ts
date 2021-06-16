40a3d2af213b92136d983aeb172c501769432b7e
yash-bytes1 <yash.sanja@bytestechnolab.in>
2021-06-16 12:03:44 +0530

    add redis and grpc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,30 @@
-import { Controller } from '@nestjs/common';
-import { MathService } from './math.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { Controller } from "@nestjs/common";
+import { MathService } from "./math.service";
+import { GrpcMethod, MessagePattern } from "@nestjs/microservices";
+
+interface INumberArray {
+  data: number[];
+}
+
+interface ISumOfNumberArray {
+  sum: number;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly mathService: MathService) {}
+  constructor(private readonly mathService: MathService) {
+  }
 
   @MessagePattern('add')
   async accumulate( data: number[]) {
     console.log(data);
     return this.mathService.accumulate(data);
   }
+
+  // @GrpcMethod("AppController", "Accumulate")
+  // accumulate(numberArray: INumberArray, metadata: any): ISumOfNumberArray {
+  //   return {
+  //     sum: this.mathService.accumulate(numberArray.data),
+  //   };
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,38 @@
-import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { Transport } from "@nestjs/microservices";
+import { AppModule } from "./app.module";
+import { join } from "path" ;
+
+// const microserviceOptions = {
+//   transport: Transport.TCP,
+//   options: {
+//     host: '127.0.0.1',
+//     port: 8877,
+//   },
+// };
 
 const microserviceOptions = {
-  transport: Transport.TCP,
-  options:{
-      host:'127.0.0.1',
-      port: 8877
-  }
-}
+  transport: Transport.REDIS,
+  options: {
+    url: 'redis://localhost:6379',
+  },
+};
+// const microserviceOptions = {
+//   transport: Transport.GRPC,
+//   options: {
+//     package: "app",
+//     protoPath: join(__dirname, "../src/app.proto")
+//   },
+// };
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions)
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions,
+  );
   app.listen(() => {
-    console.log('microservice running.');
-  })
+    console.log("microservice running.");
+  });
 }
+
 bootstrap();
